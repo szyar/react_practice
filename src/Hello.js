ae6ac94f952d742eb55215c45f6862274efe382e
szyar <soezayarszyar@gmail.com>
2020-08-31 08:42:24 +0630

    start new app hello

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hello.css";
+
+class Hello extends React.Component {
+  render() {
+    return(
+      <h1>Hello world</h1>
+    );
+  }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Game from "./Game";
+import Hello from "./Hello";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
+    <Hello />
   </React.StrictMode>,
   document.getElementById("root")
 );
